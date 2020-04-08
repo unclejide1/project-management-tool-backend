4b7e72e5174fed223c067f9e9861c5f7b1de225f
Jideofor <njusticej@gmail.com>
2020-04-08 16:14:57 +0100

    feat: new build

---
+++ src/main/resources/static/precache-manifest.e19e67dc899193e657e38d15876ddc9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af8c1632703b7c5354766cb6f5b4450",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bace8b3194b1df4efffb",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "69e899539b485d9a4067",
+    "url": "/static/css/main.86ded62d.chunk.css"
+  },
+  {
+    "revision": "bace8b3194b1df4efffb",
+    "url": "/static/js/2.7983fb89.chunk.js"
+  },
+  {
+    "revision": "0bc5220d92bfe34c3dec2f124a7bb286",
+    "url": "/static/js/2.7983fb89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69e899539b485d9a4067",
+    "url": "/static/js/main.7d763405.chunk.js"
+  },
+  {
+    "revision": "1f3d9f339fe13b1e25a2",
+    "url": "/static/js/runtime-main.44587119.js"
+  }
+]);
